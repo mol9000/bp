3946130726fd69a83d809b351e738a20638982b6
Daniel <daniel.wittenburg@stud.uni-hannover.de>
2019-01-07 13:39:49 +0100

    Remodeled the interview page

--- src/js/bp.js
+++ src/js/bp.js
@@ -1,9 +1,9 @@
-$(document).ready(function () {
+$(document).ready(function() {
     $("#sidebar").mCustomScrollbar({
         theme: "minimal"
     });
 
-    $('#sidebarCollapse').on('click', function () {
+    $('#sidebarCollapse').on('click', function() {
         $('#sidebar, #content').toggleClass('active');
         $('.collapse.in').toggleClass('in');
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
